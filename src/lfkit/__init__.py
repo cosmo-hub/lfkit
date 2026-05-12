604dc1d1399f6b25afa2ff77fbf6b1c64bd1880a
niko.sarcevic <nikolina.sarcevic@gmail.com>
2026-05-11 20:57:11 -0400

    Use setuptools-scm for automatic versions

--- src/lfkit/__init__.py
+++ src/lfkit/__init__.py
@@ -5,6 +5,12 @@ from __future__ import annotations
 from lfkit.api.corrections import Corrections
 from lfkit.api.lumfunc import LuminosityFunction
 
+try:
+    from lfkit._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
 __all__ = [
     "Corrections",
     "LuminosityFunction",
